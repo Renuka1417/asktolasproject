82ad3cb7d769a26e6e17c6f7ac81eff9d778bdf4
Renuka1417 <123815835+Renuka1417@users.noreply.github.com>
2023-03-04 17:22:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 21:16:03 2023
+
+@author: Renuka
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+with open('C:/Users/Renuka/nlp_model.pkl', 'rb') as f:
+ cv, classifier= pickle.load(f)
+#create function for prediction
+def sentiment_analysis(review):
+    X_test = [review]
+    X_test_vect = cv.transform(X_test)
+    y_pred = classifier.predict(X_test_vect)
+    print(y_pred)
+
+
+    if y_pred[0] == 1:
+        return "Positive review"
+    else:
+        return "Negative review"
+    
+    
+def main():
+    
+    #title of page
+    st.title("REVIEW ANALYSIS APP")
+    
+
+    review = st.text_input("Enter a review:")
+    if review:
+      result = sentiment_analysis(review)
+      st.write("Sentiment:", result)
+    
+    
+if __name__ == '__main__':
+    main()
+    #to run program run following code in promt
+   # streamlit run "E:\ml model deployment\app.py"
---
+++ predictivesystem.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pickle
+with open('C:/Users/Renuka/nlp_model.pkl', 'rb') as f:
+ cv, classifier= pickle.load(f)
+X_test = ["This is a neutral review."]
+X_test_vect = cv.transform(X_test)
+y_pred = classifier.predict(X_test_vect)
+print(y_pred)
+
+
+if y_pred[0] == 1:
+    print("Negative review")
+else:
+    print("Positive review")
